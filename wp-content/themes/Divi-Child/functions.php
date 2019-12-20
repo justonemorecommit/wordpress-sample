6e840983146f8c7a1a7e70c64f25eb62e44c6857
justonemorecommit <stayhungryjack@gmail.com>
2019-12-20 12:24:24 +0200

    Enable segment correct Segment code and delete old one

--- wp-content/themes/Divi-Child/functions.php
+++ wp-content/themes/Divi-Child/functions.php
@@ -95,22 +95,6 @@ register_sidebar( array(
 	'description' => __( 'This sidebar is in the bottom right corner for the badges'),
 ) );
 
-/* Segmnet code */
-function lh_add_segment_code() {
-	ob_start(); ?>
-
-	<script type="text/javascript">
-        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-            analytics.load("7cp8b9jFwyvGTugGOYY4nyD6qCDvNN5Y");
-            analytics.page();
-        }}();
-	</script>
-
-<?php echo ob_get_clean();
-}
-add_action('wp_head', 'lh_add_segment_code');
-
-
 
 // Add custom field & quickedit field
 

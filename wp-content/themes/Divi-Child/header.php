3bd5810f6896a7320a901578c94dabf1345aaeaa
justonemorecommit <stayhungryjack@gmail.com>
2020-07-09 18:22:13 +0200

    feat: completely remove internal_name

--- wp-content/themes/Divi-Child/header.php
+++ wp-content/themes/Divi-Child/header.php
@@ -24,18 +24,6 @@
 
     <?php wp_head(); ?>
 
-    <?php
-        // $internal_name = get_post_meta( get_the_ID(), 'lh_internal_name', true );
-
-        // if(!empty($internal_name)) {
-        //     $page_name_for_analytics = $internal_name;
-        // } else {
-        //     $post_type = get_post_type();
-        //     $orig_id =  apply_filters( 'wpml_object_id', get_the_ID(), $post_type, TRUE, 'de');
-        //     $page_name_for_analytics = get_the_title($orig_id);
-        // }
-    ?>
-
     <script type="text/javascript">
         !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
             analytics.load("7cp8b9jFwyvGTugGOYY4nyD6qCDvNN5Y");

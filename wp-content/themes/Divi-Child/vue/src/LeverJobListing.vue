7aed0df94abe260b720380c3d58d5d4c97a1e313
justonemorecommit <stayhungryjack@gmail.com>
2020-07-31 12:36:16 +0200

    feat: update vue build

--- wp-content/themes/Divi-Child/vue/src/LeverJobListing.vue
+++ wp-content/themes/Divi-Child/vue/src/LeverJobListing.vue
@@ -8,75 +8,74 @@
         </div>
         <a class="btn-job-application" :href="job.hostedUrl">See more
         </a>
-      </div>           
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios"
-export default {
-  name: 'LeverJobListing',
-  data () {
-    return {
-      jobList: [] 
-    }
+  import axios from "axios"
+  export default {
+    name: 'LeverJobListing',
+    data () {
+      return {
+        jobList: []
+      }
 
-  },
-  mounted () {
-    this.getData()
-  },
-  methods: {
-    getData() {
-      axios.get('https://api.lever.co/v0/postings/laserhub-2?mode=json')
+    },
+    mounted () {
+      this.getData()
+    },
+    methods: {
+      getData() {
+        axios.get('https://api.lever.co/v0/postings/laserhub-2?mode=json')
         .then( (response) => {
-      // handle success
+          // handle success
           this.jobList = response.data;
-          console.log(response);
-  })  
+        })
         .catch(function (error) {
-      // handle error
-          console.log(error);
-  })
+          // handle error
+          console.error(error);
+        })
+      }
     }
-  }
   }
 
 </script>
 
 <style lang="scss" scoped>
-  .job-container {
-    max-width: 600px;
-    margin: 0 auto;
-    h3 {
-      font-family: TTHoves-DemiBold, sans-serif;
-      font-size: 20px;
-      }
-    p {
-      font-family: TTHoves-Regular, sans-serif;
-      font-size: 16px;
-      }
-  }
-  .job-row {
-    background-color: #fff;
-    display: flex;
-    padding: 20px 30px;
-    justify-content: space-between;
-    align-items: center;
-    margin: 25px 0;
-  }
-  .btn-job-application {
-    color: #56DCAC;
-    background-color: #fff;
-    border: 3px solid #56DCAC;
-    padding: 10px 15px;
+.job-container {
+  max-width: 600px;
+  margin: 0 auto;
+  h3 {
     font-family: TTHoves-DemiBold, sans-serif;
-    text-decoration: none;
-    font-size: 16px;
-    border-radius: 2px;
+    font-size: 20px;
   }
-  .btn-job-application:hover {
-    background-color: #56DCAC;
-    color: #000;
-   }
+  p {
+    font-family: TTHoves-Regular, sans-serif;
+    font-size: 16px;
+  }
+}
+.job-row {
+  background-color: #fff;
+  display: flex;
+  padding: 20px 30px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 25px 0;
+}
+.btn-job-application {
+  color: #56DCAC;
+  background-color: #fff;
+  border: 3px solid #56DCAC;
+  padding: 10px 15px;
+  font-family: TTHoves-DemiBold, sans-serif;
+  text-decoration: none;
+  font-size: 16px;
+  border-radius: 2px;
+}
+.btn-job-application:hover {
+  background-color: #56DCAC;
+  color: #000;
+}
 </style>
